be6a64ff5384ae4a1632630f4e14de088f9f0251
James Hayward <hayward.jp@gmail.com>
2020-01-16 14:26:42 +0000

    added safeguard to prevent user duplication

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -8,15 +8,21 @@ const mongo = new MongoDB();
 const errorMsg = 'An error occurred';
 
 authRoutes.post('/register', async (req: Request, res: Response) => {
+  const user = await findUser(req.body);
+  if (user) return res.send('User already exists');
   const result = await mongo.createRecord(req.body, 'users');
   res.send(result ? result : errorMsg)
 })
 
-authRoutes.post('/login', async (req: any, res: any) => {
-  const { username } = req.body;
-  const result = await mongo.findRecord({ username }, 'users');
-  res.send(result ? result : errorMsg)
+authRoutes.post('/login', async (req: Request, res: Response) => {
+  const user = await findUser(req.body);
+  res.send(user ? user : errorMsg)
 })
 
+async function findUser(userData: any) {
+  const { username } = userData;
+  const result = await mongo.findRecord({ username }, 'users');
+  return result;
+}
 
 export { authRoutes };
